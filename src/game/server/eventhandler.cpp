7888ac05d5822997b3e191507f11d7c85a988f3d
jupeyy <jupjopjap@gmail.com>
2016-11-12 23:27:04 +0100

    check if snapping player is nullptr

--- src/game/server/eventhandler.cpp
+++ src/game/server/eventhandler.cpp
@@ -47,7 +47,7 @@ void CEventHandler::Snap(int SnappingClient)
 		if(SnappingClient == -1 || CmaskIsSet(m_aClientMasks[i], SnappingClient))
 		{
 			CNetEvent_Common *ev = (CNetEvent_Common *)&m_aData[m_aOffsets[i]];
-			if(SnappingClient == -1 || distance(GameServer()->m_apPlayers[SnappingClient]->m_ViewPos, vec2(ev->m_X, ev->m_Y)) < 1500.0f)
+			if(SnappingClient == -1 || (GameServer()->m_apPlayers[SnappingClient] && distance(GameServer()->m_apPlayers[SnappingClient]->m_ViewPos, vec2(ev->m_X, ev->m_Y))) < 1500.0f)
 			{
 				void *d = GameServer()->Server()->SnapNewItem(m_aTypes[i], i, m_aSizes[i]);
 				if(d)
