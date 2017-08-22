3d6beddaf90aacfdb43c86d89490cc8e938152a0
Jupeyy <jupjopjap@gmail.com>
2017-08-22 22:26:25 +0200

    new functions for QuadroMask

--- src/game/server/gamecontext.h
+++ src/game/server/gamecontext.h
@@ -61,6 +61,10 @@ struct QuadroMask {
 		m_Mask[3] ^= mask;
 		return *this;
 	}
+	
+	bool operator==(QuadroMask& q){
+		return m_Mask[0] == q.m_Mask[0] && m_Mask[1] == q.m_Mask[1] && m_Mask[2] == q.m_Mask[2] && m_Mask[3] == q.m_Mask[3];
+	}
 
 	int Count() {
 		int Counter = 0;
@@ -83,6 +87,10 @@ struct QuadroMask {
 			}
 		}
 		return -1;
+	}
+	
+	void SetBitOfPosition(int Pos){
+		m_Mask[Pos / 64] |= 1 << (Pos % 64);
 	}
 };
 #endif
