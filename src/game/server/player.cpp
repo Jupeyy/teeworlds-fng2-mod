19e09086d31e904a797bd5c46c46cbd15ec189bf
Jupeyy <jupjopjap@gmail.com>
2017-10-18 23:11:28 +0200

    calculate wrong spikes in player score too

--- src/game/server/player.cpp
+++ src/game/server/player.cpp
@@ -376,7 +376,7 @@ void CPlayer::CalcScore(){
 	if(g_Config.m_SvScoreDisplay == 0){
 		m_Score = m_Stats.m_Kills + m_Stats.m_Unfreezes;
 		//TODO: make this configurable
-		m_Score += (m_Stats.m_GrabsNormal * g_Config.m_SvPlayerScoreSpikeNormal) + (m_Stats.m_GrabsTeam * g_Config.m_SvPlayerScoreSpikeTeam) + (m_Stats.m_GrabsGold * g_Config.m_SvPlayerScoreSpikeGold);
+		m_Score += (m_Stats.m_GrabsNormal * g_Config.m_SvPlayerScoreSpikeNormal) + (m_Stats.m_GrabsTeam * g_Config.m_SvPlayerScoreSpikeTeam) + (m_Stats.m_GrabsFalse * g_Config.m_SvPlayerScoreSpikeFalse) + (m_Stats.m_GrabsGold * g_Config.m_SvPlayerScoreSpikeGold);
 	} else {
 		m_Score = m_Stats.m_Kills + m_Stats.m_Unfreezes - m_Stats.m_Hits;
 		//TODO: make this configurable
