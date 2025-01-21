2fe5f5821ed652de3820118fc5c463d564b4478b
Jupeyy <jupjopjap@gmail.com>
2025-01-21 21:09:40 +0100

    Fix Windows CI

--- src/base/system.c
+++ src/base/system.c
@@ -33,7 +33,6 @@
 
 #elif defined(CONF_FAMILY_WINDOWS)
 	#define WIN32_LEAN_AND_MEAN
-	#define _WIN32_WINNT 0x0501 /* required for mingw to get getaddrinfo to work */
 	#include <windows.h>
 	#include <winsock2.h>
 	#include <ws2tcpip.h>
